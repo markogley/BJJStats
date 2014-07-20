eb14a1d2d014c1fb8dd39c77799f04670acb1d1c
markogley <markogley@Marks-MacBook-Pro.local>
2014-07-20 16:08:44 +0900

    -Created delegate protocol to create basic submission object and bring the information back to the HomeViewController so that the data can be persisted. -Renamed some properties to have more straightforward naming conventions.

---
+++ BJJStats/MOSubmissionObject.h
@@ -0,0 +1,13 @@
+//
+//  MOSubmissionObject.h
+//  BJJStats
+//
+//  Created by Mark Ogley on 2014-07-20.
+//  Copyright (c) 2014 Mark Ogley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOSubmissionObject : NSObject
+
+@end
---
+++ BJJStats/MOSubmissionObject.m
@@ -0,0 +1,13 @@
+//
+//  MOSubmissionObject.m
+//  BJJStats
+//
+//  Created by Mark Ogley on 2014-07-20.
+//  Copyright (c) 2014 Mark Ogley. All rights reserved.
+//
+
+#import "MOSubmissionObject.h"
+
+@implementation MOSubmissionObject
+
+@end
