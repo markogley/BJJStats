3a0026a3008cd63a44b69edea88762e1ed2b8c34
markogley <markogley@Marks-MacBook-Pro.local>
2014-07-22 23:13:06 +0900

    - Created converter class to convert from MOSubmissionObject to PropertyList and Back. - Solved counter issue in statistics view controller so no more doubling of addedSubmissionObjects. - Made some UI tweaks to show all information at bottom of view for statistics

---
+++ BJJStats/MOObjectConverter.h
@@ -0,0 +1,13 @@
+//
+//  MOObjectConverter.h
+//  BJJStats
+//
+//  Created by Mark Ogley on 2014-07-22.
+//  Copyright (c) 2014 Mark Ogley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOObjectConverter : NSObject
+
+@end
---
+++ BJJStats/MOObjectConverter.m
@@ -0,0 +1,13 @@
+//
+//  MOObjectConverter.m
+//  BJJStats
+//
+//  Created by Mark Ogley on 2014-07-22.
+//  Copyright (c) 2014 Mark Ogley. All rights reserved.
+//
+
+#import "MOObjectConverter.h"
+
+@implementation MOObjectConverter
+
+@end
